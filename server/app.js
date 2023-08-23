f8dc645c129d5eb300cf745c0a0574dddd6d5bdd
ICE Error <94098012+rattbodice@users.noreply.github.com>
2023-08-23 12:40:27 +0700

    delete route test

--- server/app.js
+++ server/app.js
@@ -7,10 +7,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 require('./routes')(app);
 
-app.get('/test',function (req,res){
-    res.send("test");
-})
-
 let port = 8081;
 app.listen(port,()=>{
     console.log("Server is running on port : "+port);
